432b4b3ab9f42e1486254fea3752c0b202a2a820
Travis <Travisbatts6@gmail.com>
2018-07-17 15:02:51 -0400

    more dynamic WP theme

---
+++ source/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<meta charset="utf-8">
+    	<meta name="description" content="The best in cats">
+    	<meta name="keywords" content="Cats, code, lolz">
+		<title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/normalize.css">
+		<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/main.css">
+	</head>
+	<body>
+
+
+	
+		<div id="header">
+			<h1><a href="#">Cats Who Code</a></h1>
+		</div><!-- #header --> 
+
+		<div class="navcontainer">
+			<div class="inner-navcontainer">
+				<nav class="clearfix">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Cat of the Week</a></li>
+							<li><a href="#">Location</a></li>
+							<li><a href="#">About Us</a></li>
+						</ul>
+				</nav>
+			</div>
+		</div>
+
+
+
+
+		<div id="container">
+			
+			<div id="main">
---
+++ source/sidebar.php
@@ -0,0 +1,40 @@
+<div id="sidebar" class="clearfix">
+
+<div class="widget">
+    <h3>Archive</h3>
+    <ul>
+        <li><a href="#">January</a></li>
+        <li><a href="#">February</a></li>
+        <li><a href="#">March</a></li>
+        <li><a href="#">April</a></li>
+        <li><a href="#">May</a></li>
+        <li><a href="#">June</a></li>
+        <li><a href="#">July</a></li>
+    </ul>
+</div><!-- .widget --> 
+
+<div class="widget">
+    <h3>Blogroll</h3>
+    <ul>
+        <li><a href="#">Mailchimp</a></li>
+        <li><a href="#">One Munch</a></li>
+        <li><a href="#">Hacker Mews</a></li>
+        <li><a href="#">Soylent Cat Food</a></li>
+        <li><a href="#">Blue Buffalo</a></li>
+    </ul>
+</div><!-- .widget --> 
+
+<div class="widget">
+    <a href="#">
+        <img src="<?php bloginfo(template_directory); ?>/images/ad-submit-your-own.png" alt="Submit Your Own">
+    </a>
+</div><!-- .widget --> 
+
+<div class="widget">
+    <a href="#">
+        <img src="<?php bloginfo(template_directory); ?>/images/ad-tshirts.png" alt="Cats Who Code Tees">
+    </a>
+</div><!-- .widget --> 
+
+
+</div><!-- #sidebar -->
